10eb71bc87dc41622c44596bbe38feef056225a1
WilMedz <wilmamediza22@gmail.com>
2025-04-27 18:16:00 -0500

    Creación de clase Asistente

---
+++ Taller_De_Casos/src/taller_de_casos/Asistentes.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller_de_casos;
+
+/**
+ *
+ * @author percy
+ */
+public class Asistentes {
+    private String Nombre;
+    private String Apellido_Paterno;
+    private String Apellido_Materno;
+    private int Edad;
+    private int Doc_Identidad;
+    private String Tipo_Doc;
+    private String Telefono;
+    private String Correo;
+    private String Categoria;
+    
+    
+    
+    
+    
+}
