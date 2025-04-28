53e3803bd3b791259b5dd21fa0d849387932052d
Abner2025l <abnervazquezchava123@gmail.com>
2025-04-27 19:08:05 -0500

    se agrego correctamente getters and setters

--- Taller_De_Casos/src/taller_de_casos/Asistentes.java
+++ Taller_De_Casos/src/taller_de_casos/Asistentes.java
@@ -18,6 +18,78 @@ public class Asistentes {
     private String Telefono;
     private String Correo;
     private String Categoria;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido_Paterno() {
+        return Apellido_Paterno;
+    }
+
+    public void setApellido_Paterno(String Apellido_Paterno) {
+        this.Apellido_Paterno = Apellido_Paterno;
+    }
+
+    public String getApellido_Materno() {
+        return Apellido_Materno;
+    }
+
+    public void setApellido_Materno(String Apellido_Materno) {
+        this.Apellido_Materno = Apellido_Materno;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public int getDoc_Identidad() {
+        return Doc_Identidad;
+    }
+
+    public void setDoc_Identidad(int Doc_Identidad) {
+        this.Doc_Identidad = Doc_Identidad;
+    }
+
+    public String getTipo_Doc() {
+        return Tipo_Doc;
+    }
+
+    public void setTipo_Doc(String Tipo_Doc) {
+        this.Tipo_Doc = Tipo_Doc;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(String Categoria) {
+        this.Categoria = Categoria;
+    }
     
     
     
